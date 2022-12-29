12db5af894c1ee1997b29359d9a47b6dc69a1acd
omerokumus <omerokumus3@gmail.com>
2022-12-29 18:21:17 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Ömer Faruk Okumuş on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
